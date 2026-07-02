3d89aa51d1739f349b63a614a50b1331bbb68771
Yashavanth6362 <140520118+Yashavanth6362@users.noreply.github.com>
2026-07-02 11:06:06 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,11 @@
-function hello(){
-    document.getElementById("text").innerHTML="Hello BOSS! App is working.";
-}
+document.addEventListener("deviceready", function () {
+    console.log("Cordova Ready");
+
+    if (typeof Weather !== "undefined") {
+        Weather.render("#app");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("HTML Loaded");
+});
